eea999aafa7359ebe43d636a845172c44e643d82
Lynden Lim <lyndenlim15@gmail.com>
2022-02-24 21:45:00 -0500

    commit tree aaa6fe84fa7e49017a1ad3710fb3411cd0ed6568

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each { |attribute, value|
+      self.class.attr_accessor(attribute)
+      self.send("#{attribute}=", value)
+    }
+  end
 end
